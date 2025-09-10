e56b83221d4f5fe6bb918018625b4a72499b6646
SasmithaHiram <sasmithahiram2003@gmail.com>
2025-09-10 10:54:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+      <>
+          <button
+              className="px-4 py-2 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2">
+              Click Me
+          </button>
+      </>
+  )
+}
+
+export default App
